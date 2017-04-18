00f38f05c9bd7f3e2688b646ce4ae5dd34bed877
zadrok <lachlan.e.haggart@gmail.com>
2017-04-18 11:38:54 +1000

    test

--- C#-CODE/src/GameLogic.cs
+++ C#-CODE/src/GameLogic.cs
@@ -1,15 +1,15 @@
 
- 
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
- 
+
 using System.Diagnostics;
 using SwinGameSDK;
 static class GameLogic {
 
 	public static void Main() {
-		
+
 		//Opens a new Graphics Window
 		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
 
@@ -30,10 +30,3 @@ static class GameLogic {
 		GameResources.FreeResources();
 	}
 }
-
-//=======================================================
-//Service provided by Telerik (www.telerik.com)
-//Conversion powered by NRefactory.
-//Twitter: @telerik
-//Facebook: facebook.com/telerik
-//=======================================================
